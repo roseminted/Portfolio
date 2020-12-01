df24ae567d192b0aef9e96a08f3b52450e13300e
roseminted <57846775+roseminted@users.noreply.github.com>
2020-11-30 22:25:21 -0500

    home and layouts styling

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('ejs', ejsMate);
 
 app.get('/', (req, res) => {
     res.render('index')
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,4 @@
+// sidebar
 $(document).ready(function () {
     $("#sidebar").mCustomScrollbar({
         theme: "minimal"
@@ -19,3 +20,43 @@ $(document).ready(function () {
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 });
+
+//Scroll to top when arrow up clicked BEGIN
+$(window).scroll(function () {
+    const height = $(window).scrollTop();
+    if (height > 100) {
+        $('#backToTop').fadeIn();
+    } else {
+        $('#backToTop').fadeOut();
+    }
+});
+$(document).ready(function () {
+    $("#backToTop").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+
+// LETTER ANIMATIONS
+// wrap every letter in a span
+const textWrapper = document.querySelector('.banner-text .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.banner-text .letter ',
+        scale: [0, 1],
+        duration: 3000,
+        elasticity: 600,
+        delay: (el, i) => 45 * (i + 1)
+    }).add({
+        targets: '.banner-text',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 2000
+    });
+
+
